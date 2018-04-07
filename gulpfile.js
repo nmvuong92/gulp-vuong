2db743b4bb2bd36b4fc7a7a02aa5e076f27cc55e
nmvuong92 <nmvuong92@gmail.com>
2018-04-07 10:46:29 +0700

    Ya

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,24 @@
 
 var gulp = require('gulp');
-var pug = require('gulp-pug');
-var less = require('gulp-less');
 var minifyCSS = require('gulp-csso');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('html', function(){
-  return gulp.src('client/templates/*.pug')
-    .pipe(pug())
-    .pipe(gulp.dest('build/html'))
-});
+
 
 gulp.task('css', function(){
-  return gulp.src('client/templates/*.less')
-    .pipe(less())
+  return gulp.src('src/css/*.css')
+   // .pipe(less())
     .pipe(minifyCSS())
     .pipe(gulp.dest('build/css'))
 });
 
 gulp.task('js', function(){
-  return gulp.src('client/javascript/*.js')
+  return gulp.src('src/js/*.js')
     .pipe(sourcemaps.init())
     .pipe(concat('app.min.js'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('build/js'))
 });
 
-gulp.task('default', [ 'html', 'css', 'js' ]);
+gulp.task('default', ['css', 'js' ]);
